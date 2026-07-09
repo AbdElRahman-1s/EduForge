2f0aab0e99dc398a46402493ccbbb7774361c5b2
abdallah-rabie <abdallah.rabie.fayez@gmail.com>
2026-07-09 16:19:12 +0300

    feat: create custom user model (User)

--- backend/apps/accounts/models.py
+++ backend/apps/accounts/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
 
 # Create your models here.
+class User(AbstractUser):
+    email = models.EmailField(max_length=254, unique=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    REQUIRED_FIELDS = ["email"]
+
+    def __str__(self):
+        return self.username
