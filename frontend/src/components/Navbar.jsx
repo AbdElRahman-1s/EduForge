8da4e61b6109983840601b9220387decece93e90
AbdElRahman <xinfargo@gmail.com>
2026-07-17 17:38:50 +0300

    Finish navbar v1

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,39 +1,91 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { AuthContext } from "../context/AuthContext";
-import axios from "axios";
+import { IoBookOutline } from "react-icons/io5";
+import { IoIosNotificationsOutline, IoIosArrowDown,  IoIosArrowUp} from "react-icons/io";
+import { FiLogOut } from "react-icons/fi";
+import { CiSettings } from "react-icons/ci";
+
+import './navbar.css';
 
 function Navbar() {
-  
 
-  const { logout , accessToken} = useContext(AuthContext);
+  const [showProfileList , setShowProfileList] = useState(false);
+
+
+  const { logout } = useContext(AuthContext);
   const navigate = useNavigate();
 
-  async function handleLogout() {
-    try {
-      await axios.post(
-        "/api/auth/logout/",
-        {},{
-         headers: {
-          Authorization: `Bearer ${accessToken}`,
-        }},
-        {
-          withCredentials: true,
-        }
-      );
-    } finally {
-      logout();
-      navigate("/");
-    }
+  function handleLogout() {
+    logout();
+    navigate('/auth')
   }
 
 
 
   return (
     <>
-    
-    <div>Navbar</div>
-    <button onClick={handleLogout}>Logout</button>
+      <nav>
+
+        <div className="nav-div">
+
+          <div className="left-section">
+            <div className="title">
+              <span><IoBookOutline /></span>
+              <h2>EduForge</h2>
+            </div>
+          </div>
+
+          <div className="middle-section">
+            <span>Browse</span>
+            <span>My Courses</span>
+
+          </div>
+
+
+          <div className="right-section">
+
+            <div className="notification-div">
+              <span className="notification-icon">
+                <IoIosNotificationsOutline />
+              </span>
+              <span className="notification-num">
+                4
+              </span>
+            </div>
+
+            <div 
+            className="profile"
+            onClick={() => {setShowProfileList(!showProfileList)}}
+            >
+              <img src="https://media.istockphoto.com/id/2151669184/vector/vector-flat-illustration-in-grayscale-avatar-user-profile-person-icon-gender-neutral.jpg?s=612x612&w=0&k=20&c=UEa7oHoOL30ynvmJzSCIPrwwopJdfqzBs0q69ezQoM8=" alt="" />
+              {!showProfileList ? <IoIosArrowDown/> : <IoIosArrowUp />  }
+            </div>
+
+            <div className={showProfileList ? "more-info" : "hide-info"}>
+
+              <div className="move-to">
+                <CiSettings />
+                <span>Move to</span>
+              </div>
+              <div className="logout" onClick={handleLogout}>
+                <FiLogOut />
+                <span>
+                  Sign out
+                </span>
+              </div>
+
+            </div>
+
+          </div>
+
+
+
+
+        </div>
+
+      </nav>
+
     </>
   )
 }
