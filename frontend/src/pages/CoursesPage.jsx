680d9980980cbb030ee35221d9f04fa954f3ce0c
AbdElRahman <xinfargo@gmail.com>
2026-07-21 16:25:55 +0300

    Finish navDashboard

--- frontend/src/pages/CoursesPage.jsx
+++ frontend/src/pages/CoursesPage.jsx
@@ -3,19 +3,20 @@ import HeroCourses from '../components/HeroCourses';
 import Categories from '../components/Categories';
 import Statistics from '../components/Statistics';
 import CourseSection from '../components/CourseSection';
+import Navbar from '../components/Navbar';
 function CoursesPage() {
   return (
 
     <>
+      <Navbar />
 
+      <div className='courses-body'>
 
-    <div className='courses-body'>
-
-      <HeroCourses />
-      <Categories />
-      <Statistics />
-      <CourseSection />
-    </div>
+        <HeroCourses />
+        <Categories />
+        <Statistics />
+        <CourseSection />
+      </div>
     </>
   )
 }
