a53e7518609a16aabca0d92edee7122b5ca857f6
AbdElRahman <xinfargo@gmail.com>
2026-07-25 18:07:08 +0300

    Finish Dashboard page ui

---
+++ frontend/src/pages/dashboard-page/Dashboard.jsx
@@ -0,0 +1,69 @@
+// import { useContext } from "react";
+// import { AuthContext } from "../context/AuthContext";
+// import { Link } from "react-router-dom";
+import { useState } from "react";
+import NavDashboard from "../../components/dashboard-page-components/nav-dashboard/NavDashboard";
+import Sidebar from "../../components/dashboard-page-components/sidebar/Sidebar";
+
+import './dashboard.css'
+import DashboardStats from "../../components/dashboard-page-components/dashboard-stats/DashboardStats";
+import RecentActivity from "../../components/dashboard-page-components/recent-activity/RecentActivity";
+import CourseToolbar from "../../components/dashboard-page-components/course-toolbar/CourseToolbar";
+import CoursesActions from "../../components/dashboard-page-components/course-actions/CoursesActions";
+import UsersComponent from "../../components/dashboard-page-components/users-component/UsersComponent";
+function Dashboard() {
+
+    //     const { user } = useContext(AuthContext);
+
+    const [switchSidebar, setSwitchSidebar] = useState(true);
+    const [selected, setSelected] = useState('dashboard');
+
+
+    //  if(!user) return <h2></h2>
+
+
+    return (
+        <>
+            <Sidebar
+                switchSidebar={switchSidebar}
+                setSwitchSidebar={setSwitchSidebar}
+                selected={selected}
+                setSelected={setSelected}
+            />
+            <div className={switchSidebar ? "dashboard-container-big" : "dashboard-container-sm"}>
+                <NavDashboard
+                    switchSidebar={switchSidebar}
+                    selected={selected}
+                />
+
+                <div className="dashboard-body">
+                    {
+                    selected === 'dashboard' &&
+                    <>
+                    <DashboardStats />
+                    <RecentActivity />
+                    </>
+                    }
+                    {
+                    selected === 'courses' &&
+                    <>
+                    <CourseToolbar />
+                    <CoursesActions />
+                    </>
+                    }
+                    {
+                    selected === 'users' &&
+                    <>
+                      <UsersComponent />
+                    </>
+                    }
+
+                </div>
+            </div>
+        </>
+
+    )
+
+}
+
+export default Dashboard;
