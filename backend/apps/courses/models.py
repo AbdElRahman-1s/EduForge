e52d840dc3ccea46256f277aaaf151a47f2c7f84
abdallah-rabie <abdallah.rabie.fayez@gmail.com>
2026-07-30 08:55:29 +0300

    feat(courses): add category model

--- backend/apps/courses/models.py
+++ backend/apps/courses/models.py
@@ -1,7 +1,30 @@
 from django.db import models
 from django.conf import settings
+from django.utils.text import slugify
+from django.db.models.functions import Lower, Trim
 
 # Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                Lower(Trim("name")), name="unique_lower_trimmed_category_name"
+            )
+        ]
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+
+        return super().save(*args, **kwargs)
 
 
 class Course(models.Model):
@@ -9,6 +32,9 @@ class Course(models.Model):
     description = models.TextField()
     instructor = models.ForeignKey(
         settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="courses"
+    )
+    category = models.ForeignKey(
+        Category, on_delete=models.PROTECT, related_name="courses"
     )
 
     published = models.BooleanField(default=False)
