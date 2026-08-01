226422ada21f098c641b4335f750cb502e7badf5
abdallah-rabie <abdallah.rabie.fayez@gmail.com>
2026-08-01 07:40:53 +0300

    feat: add section and lesson models

--- backend/apps/courses/models.py
+++ backend/apps/courses/models.py
@@ -89,3 +89,28 @@ class Course(models.Model):
                 condition=Q(price__gte=0), name="price_must_be_non_negative"
             )
         ]
+
+
+class Section(models.Model):
+    course = models.ForeignKey(
+        Course, on_delete=models.CASCADE, related_name="sections"
+    )
+    title = models.CharField(max_length=150)
+    order = models.PositiveSmallIntegerField()
+
+    def __str__(self):
+        return self.title
+
+
+class Lesson(models.Model):
+    section = models.ForeignKey(
+        Section, on_delete=models.CASCADE, related_name="lessons"
+    )
+    title = models.CharField(max_length=150)
+    duration_seconds = models.PositiveIntegerField()
+    video = models.URLField()
+    free = models.BooleanField(default=False)
+    order = models.PositiveSmallIntegerField()
+
+    def __str__(self):
+        return self.title
