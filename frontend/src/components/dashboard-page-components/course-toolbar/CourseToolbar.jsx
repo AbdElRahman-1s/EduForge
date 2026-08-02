519a662211c385cc6de076112b99cec120d4869e
AbdElRahman <xinfargo@gmail.com>
2026-08-02 16:47:41 +0300

    Finish add course integration by formData with rest of ui & styles

--- frontend/src/components/dashboard-page-components/course-toolbar/CourseToolbar.jsx
+++ frontend/src/components/dashboard-page-components/course-toolbar/CourseToolbar.jsx
@@ -1,6 +1,6 @@
-import { useContext, useState } from "react";
+import { useContext, useState, useEffect } from "react";
 import { FiPlusCircle } from "react-icons/fi";
-import { IoIosSearch , IoMdClose} from "react-icons/io";
+import { IoIosSearch, IoMdClose } from "react-icons/io";
 import { AiOutlinePicture } from "react-icons/ai";
 import { LiaFileVideo } from "react-icons/lia";
 
@@ -9,50 +9,134 @@ import axios from "axios";
 import { AuthContext } from "../../../context/AuthContext";
 function CourseToolbar() {
 
-  const {accessToken} = useContext(AuthContext);
 
-  async function postAddCourse(){
-    try{
+
+
+  const [addCourse, setAddCourse] = useState(false);
+  const [addedCourseDetails, setAddedCourseDetails] = useState({});
+  const [disabeldEffect, setDisabeldEffect] = useState(false);
+  const [fetchCategories, setFetchCategories] = useState([]);
+  const [fetchTopics, setFetchTopics] = useState([]);
+  const [errorMessage, setErrorMessage] = useState("note-p-hide");
+
+
+
+
+
+  const [title, setTitle] = useState("");
+  const [selectedTopics, setSelectedTopics] = useState([]);
+  const [description, setDescription] = useState("");
+  const [categorySelect, setCategorySelect] = useState("");
+  const [levelSelect, setLevelSelect] = useState("");
+  const [price, setPrice] = useState("");
+  const [published, setPublished] = useState(false);
+  const [badgeSelect, setBadgeSelect] = useState(null);
+  const [thumbnail, setThumbnail] = useState(null);
+
+
+
+
+  const { accessToken } = useContext(AuthContext);
+
+  async function postAddCourse() {
+    try {
 
       setDisabeldEffect(true);
 
-    const response = await axios.post('/api/courses/',
-      {
-        title,
-        description
-      },
-      {
-       headers:{
-        Authorization: `Bearer ${accessToken}`
-       },
-        withCredentials:true,
-       
+      const formData = new FormData();
+
+
+      formData.append("title", title);
+      selectedTopics.forEach((topicId) => {
+        formData.append("topics", Number(topicId));
+      });
+      formData.append("description", description);
+      formData.append("level", levelSelect);
+      formData.append("category", categorySelect);
+      formData.append("price", price);
+      formData.append("badge", badgeSelect);
+      formData.append("thumbnail", thumbnail);
+      formData.append("published", published);
+
+
+
+      const response = await axios.post('/api/courses/',
+        formData,
+        {
+          headers: {
+            Authorization: `Bearer ${accessToken}`
+          },
+          withCredentials: true,
+
+        }
+      );
+
+
+      setAddedCourseDetails(response.data);
+      console.log(response.data);
+
+
+    } catch (error) {
+      console.log(error.response?.data);
+      if (error.response?.data) {
+        setErrorMessage("note-p");
       }
-    );
-
-    
-    setAddedCourseDetails(response.data);
-    console.log(response.data);
-    
-
-    }catch(error){
-      console.log(error);
-      
-    }finally{
+    } finally {
       setDisabeldEffect(false);
     }
 
   }
 
 
+  useEffect(() => {
+    async function fetchCategories() {
+      const response = await axios.get(' api/categories/',
+        {
+          headers: {
+            Authorization: `Bearer ${accessToken}`
+          }
+        }
+      );
 
-  const [addCourse, setAddCourse] = useState(false);
-  const [addedCourseDetails , setAddedCourseDetails] = useState({});
-  const [title , setTitle] = useState("");
-  const [description , setDescription] = useState("");
-  const [categorySelect, setCategorySelect] = useState("");
-  const [levelSelect, setLevelSelect] = useState("");
-  const [disabeldEffect,setDisabeldEffect] = useState(false);
+      setFetchCategories(response.data.results);
+      console.log(response.data);
+    }
+    async function fetchTopics() {
+      const response = await axios.get(' api/topics/',
+        {
+          headers: {
+            Authorization: `Bearer ${accessToken}`
+          }
+        }
+      );
+
+      setFetchTopics(response.data.results);
+      console.log(response.data);
+
+    }
+    fetchCategories();
+    fetchTopics();
+  }, [accessToken]);
+
+
+
+  function handleTopicClick(id) {
+    if (selectedTopics.includes(id)) {
+      setSelectedTopics(
+        selectedTopics.filter((topicId) => topicId !== id)
+      );
+    } else {
+      setSelectedTopics([...selectedTopics, id]);
+    }
+  }
+
+
+
+
+
+
+
+
   return (
 
     <section>
@@ -76,30 +160,44 @@ function CourseToolbar() {
                   <div className="top-desc">
                     <h2>Add New Course</h2>
                     <span
-                    onClick={() => { setAddCourse(false) }}
+                      onClick={() => { setAddCourse(false) }}
                     ><IoMdClose /></span>
                   </div>
                   <div className="title-div">
                     <label>Course Title</label>
                     <input
-                     onChange={(e) => {setTitle(e.target.value)}}
-                     type="text" placeholder="Complete React Bootcamp" />
+                      onChange={(e) => { setTitle(e.target.value) }}
+                      type="text" placeholder="Complete React Bootcamp" />
                   </div>
-                  <div className="ins-div">
-                    <label>Instructor Name</label>
-                    <input type="text" placeholder="Marcus Reid" />
+                  <div className="topics-div">
+                    <div className="topics-container">
+                      {fetchTopics.map((topic) => (
+                        <button
+                          key={topic.id}
+                          type="button"
+                          className={`topic-btn ${selectedTopics.includes(topic.id) ? "active" : ""
+                            }`}
+                          onClick={() => handleTopicClick(topic.id)}
+                        >
+                          {topic.name}
+                        </button>
+                      ))}
+                    </div>
                   </div>
                   <div className="category-level-div">
                     <select
                       value={categorySelect}
                       onChange={(e) => setCategorySelect(e.target.value)}
-                      className="category-select">
+                      className="category-select"
+                    >
                       <option value="">Choose Category</option>
-                      <option value="development">Development</option>
-                      <option value="data science">Data Science</option>
-                      <option value="design">Design</option>
-                      <option value="business">Business</option>
-                      <option value="Marketing">Marketing</option>
+                      {fetchCategories.map((category) => (
+                        <option key={category.id}
+                          value={category.id}
+                        >
+                          {category.name}
+                        </option>
+                      ))}
                     </select>
                     <select
                       value={levelSelect}
@@ -112,38 +210,87 @@ function CourseToolbar() {
                       <option value="all levels">All levels</option>
                     </select>
                   </div>
+                  <div>
+                    <select
+                      value={badgeSelect}
+                      onChange={(e) => setBadgeSelect(e.target.value)}
+                      className="badge-select">
+                      <option value="">Choose Badge</option>
+                      <option value="none">None</option>
+                      <option value="new">New</option>
+                      <option value="hot">Hot</option>
+                      <option value="bestseller">Bestseller</option>
+                    </select>
+                  </div>
 
                   <div className="price-div">
                     <label>Price(USD)</label>
-                    <input type="number" placeholder="89" />
+                    <input
+                      onChange={(e) => { setPrice(e.target.value) }}
+                      type="number" placeholder="89" />
                   </div>
 
                   <div className="desc-div">
                     <label>Description</label>
                     <textarea
-                    onChange={(e) => {setDescription(e.target.value)}}
-                    className="area-desc" 
-                    placeholder="Brief course description"></textarea>
+                      onChange={(e) => { setDescription(e.target.value) }}
+                      className="area-desc"
+                      placeholder="Brief course description"></textarea>
                   </div>
                   <div className="big-div-uploads">
-                    <div className="thumb-div">
-                      <AiOutlinePicture /> Upload Thumbnail
-                    </div>
+                    <label htmlFor="thumbnail" className="thumb-div">
+                      <AiOutlinePicture />
+                      {thumbnail ? thumbnail.name : "Upload Thumbnail"}
+                    </label>
+                    <input
+                      id="thumbnail"
+                      type="file"
+                      accept="image/*"
+                      hidden
+                      onChange={(e) => {
+                        const file = e.target.files[0];
+                        setThumbnail(file);
+                      }}
+                    />
                     <div className="video-div">
                       <LiaFileVideo /> Upload Video/URL
                     </div>
                   </div>
 
+                  <div className="publish-div">
+                    <label>
+                      <input
+                        type="radio"
+                        name="published"
+                        checked={published === false}
+                        onChange={() => setPublished(false)}
+                      />
+                      Draft
+                    </label>
+                    <label>
+                      <input
+                        type="radio"
+                        name="published"
+                        checked={published === true}
+                        onChange={() => setPublished(true)}
+                      />
+                      Published
+                    </label>
+                  </div>
+                  <p className={errorMessage}>
+                    ⚠️Note:
+                      Please fill everything correctly before adding a new course.
+                    </p>
                   <div className="btns-div">
                     <button
-                    className="cancel-btn"
-                     onClick={() => { setAddCourse(false) }}>
+                      className="cancel-btn"
+                      onClick={() => { setAddCourse(false) }}>
                       Cancel
                     </button>
                     <button
-                    disabled={disabeldEffect}
-                    onClick={postAddCourse}
-                     className="add-new-btn">
+                      disabled={disabeldEffect}
+                      onClick={postAddCourse}
+                      className="add-new-btn">
                       Add Course
                     </button>
                   </div>
