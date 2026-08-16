03e14f3d54875dbd5a1065109ed6a1777f0c5b84
AbdElRahman <xinfargo@gmail.com>
2026-08-16 12:31:42 +0300

    fetch mine enrollments & routes to course details

--- frontend/src/pages/my-courses-page/MyCourses.jsx
+++ frontend/src/pages/my-courses-page/MyCourses.jsx
@@ -1,70 +1,64 @@
 import Navbar from '../../components/Navbar';
-
+import { Link } from 'react-router-dom';
 import { FaPlay } from "react-icons/fa";
 import { FaStar } from "react-icons/fa6";
 import { IoTimeOutline, IoBookOutline } from "react-icons/io5";
 
 
 import './my-courses.css';
+import { useContext, useEffect, useState } from 'react';
+import axios from 'axios';
+import { AuthContext } from '../../context/AuthContext';
 
-const detailsCourseCard = [
-  {
-    absolute: 'Bestseller',
-    img: 'https://images.unsplash.com/photo-1555066931-4365d14bab8c?w=600&h=340&fit=crop&auto=format',
-    category: 'Development',
-    title: 'Complete React & Next.js Development',
-    instructor: 'Marcus Reid',
-    rating: '4.9',
-    numrating: '(48.2k)',
-    duration: '62',
-    lessons: '148',
-    level: 'Beginner',
-    price: '$89',
-    id: crypto.randomUUID()
-  },
-  {
-    absolute: 'Bestseller',
-    img: 'https://images.unsplash.com/photo-1555066931-4365d14bab8c?w=600&h=340&fit=crop&auto=format',
-    category: 'Development',
-    title: 'Complete React & Next.js Development',
-    instructor: 'Marcus Reid',
-    rating: '4.9(48.2k)',
-    duration: '62',
-    lessons: '148',
-    level: 'Beginner',
-    price: '$89',
-    id: crypto.randomUUID()
-  },
-  {
-    absolute: 'Bestseller',
-    img: 'https://images.unsplash.com/photo-1555066931-4365d14bab8c?w=600&h=340&fit=crop&auto=format',
-    category: 'Development',
-    title: 'Complete React & Next.js Development',
-    instructor: 'Marcus Reid',
-    rating: '4.9(48.2k)',
-    duration: '62',
-    lessons: '148',
-    level: 'Beginner',
-    price: '$89',
-    id: crypto.randomUUID()
-  },
-  {
-    absolute: 'Bestseller',
-    img: 'https://images.unsplash.com/photo-1555066931-4365d14bab8c?w=600&h=340&fit=crop&auto=format',
-    category: 'Development',
-    title: 'Complete React & Next.js Development',
-    instructor: 'Marcus Reid',
-    rating: '4.9(48.2k)',
-    duration: '62',
-    lessons: '148',
-    level: 'Beginner',
-    price: '$89',
-    id: crypto.randomUUID()
-  },
 
-]
+
+
+
 
 function MyCourses() {
+
+  const {accessToken} = useContext(AuthContext);
+
+const [myCourses,setMyCourses] = useState({
+  results:[],
+})
+
+
+
+
+  useEffect(() => {
+
+    async function fetchMyCourses() {
+
+      try {
+
+        const response = await axios.get(`/api/enrollments/mine/`,
+          {
+            headers:{
+              Authorization: `Bearer ${accessToken}`,
+            }
+          }
+        )
+
+        console.log(response.data);
+        setMyCourses(response.data);
+
+      } catch (error) {
+        console.log(error.response.data);
+
+      }
+
+
+    }
+
+    fetchMyCourses();
+
+  },[accessToken])
+
+
+
+
+
   return (
     <>
       <Navbar />
@@ -78,105 +72,108 @@ function MyCourses() {
             </p>
           </div>
 
-         <div className='course-cards'>
-        
-                  {
-                    detailsCourseCard.map((deatailsCard) => {
-                      return (
-        
-                        <div key={deatailsCard.id} className='course-card'>
-        
-                          <div className="imgs-course">
-                            <img src={deatailsCard.img} alt="big course image" />
-                            <div className="big-play-abs">
-        
-                              <div className="play-div">
-                                <FaPlay />
-        
-                              </div>
-        
-                            </div>
-        
-                            <div className="abs-catch">{deatailsCard.absolute}</div>
-        
-                            <div className="big-abs-bar">
-                              <div className="back-bar">
-                                <div className="front-bar">
-        
-                                </div>
-                              </div>
-        
-                              <div className="text-bar">68% complete</div>
-                            </div>
-        
-                          </div>
-        
-        
-        
-                          <div className="details-course">
-        
-                            <div className="category-div">{deatailsCard.category}</div>
-        
-        
-                            <h3 className="title">{deatailsCard.title}</h3>
-        
-        
-                            <div className="creator">{deatailsCard.instructor}</div>
-        
-                            <div className="rating-div">
-                              <FaStar />
-                              <FaStar />
-                              <FaStar />
-                              <FaStar />
-                              <span className="star-num">4.9</span>
-                              <span className="num-rating">(48.2k)</span>
-                            </div>
-        
-        
-                            <div className="course-stats">
-                              <span>
-                                <IoTimeOutline />
-                                62
-                              </span>
-                              <span>
-                                <IoBookOutline />
-                                148 Lessons
-                              </span>
-                              <span>
-                                Beginner
-                              </span>
-                            </div>
-        
-        
-                            <div className="course-action">
-        
-                              <div className="price-div">
-                                <span className="price">
-                                  $89
-                                </span>
-        
-                              </div>
-                              <button className="continue-btn">
-                                Continue
-                              </button>
-        
-                            </div>
-        
-        
-        
-        
-                          </div>
-        
-        
-        
+          <div className='course-cards'>
+
+            {
+              myCourses.results.map((deatailsCard) => {
+                return (
+                  <Link
+                  key={deatailsCard.id}
+                  className="link"
+                  to={`/course/details/${deatailsCard.id}`}>
+                  <div  className='course-card'>
+
+                    <div className="imgs-course">
+                      <img src={deatailsCard.thumbnail} alt="big course image" />
+                      <div className="big-play-abs">
+
+                        <div className="play-div">
+                          <FaPlay />
+
                         </div>
-        
-                      )
-                    })
-                  }
-        
-        
-                </div>
+
+                      </div>
+
+                      <div className="abs-catch">{deatailsCard.absolute}</div>
+
+                      <div className="big-abs-bar">
+                        <div className="back-bar">
+                          <div className="front-bar">
+
+                          </div>
+                        </div>
+
+                        <div className="text-bar">68% complete</div>
+                      </div>
+
+                    </div>
+
+
+
+                    <div className="details-course">
+
+                      <div className="category-div">{deatailsCard.category}</div>
+
+
+                      <h3 className="title">{deatailsCard.title}</h3>
+
+
+                      <div className="creator">{deatailsCard.instructor.username}</div>
+
+                      <div className="rating-div">
+                        <FaStar />
+                        <FaStar />
+                        <FaStar />
+                        <FaStar />
+                        <span className="star-num">4.9</span>
+                        <span className="num-rating">(48.2k)</span>
+                      </div>
+
+
+                      <div className="course-stats">
+                        <span>
+                          <IoTimeOutline />
+                          62
+                        </span>
+                        <span>
+                          <IoBookOutline />
+                          148 Lessons
+                        </span>
+                        <span>
+                          Beginner
+                        </span>
+                      </div>
+
+
+                      <div className="course-action">
+
+                        <div className="price-div">
+                          <span className="price">
+                            $89
+                          </span>
+
+                        </div>
+                        <button className="continue-btn">
+                          Continue
+                        </button>
+
+                      </div>
+
+
+
+
+                    </div>
+
+
+
+                  </div>
+                  </Link>
+                )
+              })
+            }
+
+
+          </div>
 
 
         </div>
